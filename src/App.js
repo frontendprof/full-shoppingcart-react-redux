3454523607787c4f167c12ef9037a542bec8e6ef
frontendprof <https://github.com/frontendprof>
2021-02-16 20:58:53 +0500

    Filter component creation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Filter from './components/Filter';
 import Products from './components/Products';
 import data from "./data.json";
 
@@ -29,6 +30,7 @@ class App extends React.Component{
           <div className="content">
 
             <div className="main">
+              <Filter count={this.state.products.length}/>
               <Products products={this.state.products}/>
             </div>
 
