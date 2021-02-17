b96f556af3e4c09091c8680f18e5646fb4fb7035
frontendprof <https://github.com/frontendprof>
2021-02-17 15:38:35 +0500

    Creating cart component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
 import data from "./data.json";
@@ -12,10 +13,28 @@ class App extends React.Component{
     super();
     this.state={
       products:data.products,
+      cartItems:[],
       size:"",
       sort:""
     }
   } 
+
+
+  addToCart=product=>{
+    const cartItems=this.state.cartItems.slice();
+    let alreadyInCart=false;
+    cartItems.forEach(item=>{
+      if(item._id===product._id){
+        item.count++;
+        alreadyInCart=true;
+      }
+    })
+    if(!alreadyInCart){
+      cartItems.push({...product,count:1})
+    }
+
+    this.setState({cartItems})
+  }
 
   filterProducts=(e)=>{
     if(e.target.value===""){
@@ -65,11 +84,11 @@ class App extends React.Component{
                 sortProducts={this.sortProducts}
 
               />
-              <Products products={this.state.products}/>
+              <Products products={this.state.products} addToCart={this.addToCart}/>
             </div>
 
             <div className="sidebar">
-              Cart Items
+              <Cart cartItems={this.state.cartItems}/>
             </div>
             
           </div>
